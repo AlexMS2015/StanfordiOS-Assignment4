d0f6ced3d3cbd7edafcab1df65ab41e8b79b6b2d
Alex Smith <Alex@Alexs-MacBook-Pro-3.local>
2015-04-11 16:15:24 +1000

    Custom view added (new branch)

---
+++ Matchismo/PlayingCardView.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardView.h
+//  Assignment 4
+//
+//  Created by Alex Smith on 11/04/2015.
+//  Copyright (c) 2015 Game House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayingCardView : UIView
+
+@end
