2c255a55feeefe967074277daaf15a0a3c16e516
Alex Smith <Alex@Alexs-MacBook-Pro-3.local>
2015-04-11 16:50:27 +1000

    Added PlayingCardView code

--- Matchismo/PlayingCardView.h
+++ Matchismo/PlayingCardView.h
@@ -10,4 +10,11 @@
 
 @interface PlayingCardView : UIView
 
+@property (nonatomic) NSUInteger rank;
+@property (strong, nonatomic) NSString *suit;
+
+@property (nonatomic) BOOL faceUp;
+
+- (void)pinch:(UIPinchGestureRecognizer *)gesture;
+
 @end
